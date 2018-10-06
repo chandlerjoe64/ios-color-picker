59866a4bef6b6cc0d5873b684265834f0f274dc3
Joe Chandler <jacvzb@mail.missouri.edu>
2018-10-05 21:58:58 -0500

    initial commit

---
+++ Color Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color Picker
+//
+//  Created by Joseph Chandler on 10/5/18.
+//  Copyright © 2018 Joseph Chandler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
